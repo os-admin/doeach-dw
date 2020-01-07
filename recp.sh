f5dabcbf6c4f08cd870c52781421b596f35ba983
doeach-dw <doeach@qq.com>
2020-01-07 15:15:53 +0800

    recp
    Resolve #1

---
+++ recp.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+if [ $# -eq 0 ];then
+    echo "false"
+    exit 0
+fi
+
+os=`uname -s`
+
+case $os in 
+Linux)
+    cp="xclip -sel clip"
+    ;;
+Darwim)
+    cp="pbcopy"
+    ;;
+*)
+    echo "error"
+    exit 1
+    ;;
+esac
+
+for fn in $@
+do
+    case $fn in 
+    *.py | *.sh)
+        comment="# $fn"
+        ;;
+    *.c)
+        comment="// $fn"
+        ;;
+    *.java)
+        comment="// $fn"
+        ;;
+    *.html)
+        comment="<!--...--> $fn"
+        ;;
+    esac
+
+    echo "$comment"
+    cat $fn
+    echo
+done | $cp
